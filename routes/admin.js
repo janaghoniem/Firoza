0fc2e366ea313ad05d9a4ab3dd57c9659281b44d
habiba-darwish <habibadarwish2004@gmail.com>
2024-06-19 02:25:25 +0300

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -92,16 +92,6 @@ const upload = multer({ storage: storage });
 
 // Use upload.single('collectionImage') in the route handling the form submission
 router.post('/addCollection', upload.single('collectionImage'), adminController.addCollection);
-
-//route ll orders "admin pov"
-// router.get('/orders', async (req, res, next) => {
-//     try {
-//         await adminController.getOrders(req, res);
-//     } catch (err) {
-//         next(err);
-//     }
-// });
-
 router.get('/orders', adminController.getOrders);
 
 
