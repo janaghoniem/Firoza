18b40cf41bcd132405b9e5494089c6c553ee9594
shahdcode <134103174+shahdcode@users.noreply.github.com>
2024-06-20 02:59:00 +0300

    schema updated

--- models/Customization.js
+++ models/Customization.js
@@ -2,20 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 
-const productSchema = new Schema({
-    collection_id: {
-        type: String,
-        required: true
-    },
-    name: {
-        type: String,
-        required: true
-    },
-    description: {
-        type: String,
-        required: true
-    },
-    category: {
+const CustomizeSchema = new Schema({
+    customize_id: {
         type: String,
         required: true
     },
@@ -27,26 +15,15 @@ const productSchema = new Schema({
         type: String, // Assuming it's a URL or file path to the image
         required: true
     },
-    sizes: [sizeQuantitySchema], // Array of size and quantity objects
-    rating: {
-        type: Number,
-        required: false,
-        default: 0
-    },
-    material: {
+    Stone: {
         type: String,
         required: true
     },
     color: {
         type: String,
         required: true
-    },
-    no_sales: {
-        type: Number,
-        required: false,
-        default: 0
     }
 }, { timestamps: true });
 
-const Product = mongoose.model('Product', productSchema);
-module.exports = Product;
+const custom = mongoose.model('CustomizationRing', CustomizeSchema);
+module.exports = custom;
