a8448bbdc3eeccdecc6a202f6cb93e01f1058b3c
hala-155 <140650349+hala-155@users.noreply.github.com>
2024-06-20 02:09:38 +0300

    rev

--- public/Javascript/myAccount.js
+++ public/Javascript/myAccount.js
@@ -36,3 +36,81 @@ function confirmCancel() {
     }
     closePopup();
 }
+function openReviewPopup(orderId) {
+    const popup = document.getElementById("review-popup");
+    popup.style.display = "block";
+    popup.dataset.orderId = orderId;
+
+    const stars = popup.querySelectorAll('.review-rating .fa-star');
+    stars.forEach(star => {
+        star.classList.remove('checked');
+    });
+}
+
+function closeReviewPopup() {
+    const popup = document.getElementById("review-popup");
+    popup.style.display = "none";
+    popup.dataset.orderId = "";
+
+    const comment = document.getElementById("review-comment");
+    comment.value = "";
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const stars = document.querySelectorAll('.review-rating .fa-star');
+    stars.forEach(star => {
+        star.addEventListener('click', function() {
+            const rating = this.getAttribute('data-rating');
+            setRating(rating);
+        });
+    });
+});
+
+function setRating(rating) {
+    const popup = document.getElementById("review-popup");
+    const stars = popup.querySelectorAll('.review-rating .fa-star');
+    stars.forEach(star => {
+        const starRating = star.getAttribute('data-rating');
+        if (starRating <= rating) {
+            star.classList.add('checked');
+        } else {
+            star.classList.remove('checked');
+        }
+    });
+    popup.dataset.rating = rating;
+}
+
+async function submitReview() {
+    const popup = document.getElementById("review-popup");
+    const orderId = popup.dataset.orderId;
+    const rating = popup.dataset.rating;
+    const comment = document.getElementById("review-comment").value;
+
+    
+    if (orderId && rating && comment) {
+        try {
+            const response = await fetch(`/user/orders/${orderId}/reviews`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    orderId: orderId,
+                    rating: rating,
+                    comment: comment
+                })
+            });
+            const result = await response.json();
+            alert(result.message);
+            if (result.success) {
+                closeReviewPopup();
+                window.location.reload();
+            }
+        } catch (error) {
+            console.error('Error submitting review:', error);
+            alert('Error submitting review');
+        }
+    } else {
+        alert('Please provide a rating and a comment');
+    }
+}
