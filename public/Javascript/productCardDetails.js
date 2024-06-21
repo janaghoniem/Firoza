c03b9b3f746e995149c3bd6852b63d84fd089773
janaghoniem <jana2208912@miuegypt.edu.eg>
2024-06-21 10:29:10 +0300

    css edit product page

--- public/Javascript/productCardDetails.js
+++ public/Javascript/productCardDetails.js
@@ -54,79 +54,88 @@ document.addEventListener("DOMContentLoaded",function(){
 
    
 // }
-async function addToCart(productId, price) {
-    try {
-        const response = await fetch('/user/add-to-cart', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ productId, price })
-        });
+// async function addToCart(productId, price) {
+//     try {
+//         const response = await fetch('/user/add-to-cart', {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             },
+//             body: JSON.stringify({ productId, price })
+//         });
 
-        if (response.ok) {
-            const result = await response.json();
+//         if (response.ok) {
+//             const result = await response.json();
 
-            // showPopup('Product added to cart successfully!');
+//             // showPopup('Product added to cart successfully!');
 
-            var popup = document.createElement("div");
-    popup.classList.add("popupMessage");
-    popup.textContent= "Added to cart";
-    document.body.appendChild(popup);
-
-   
-
-    setTimeout(function(){
-        popup.style.display="block";
-    },100);
-
-    setTimeout(function(){
-        popup.style.display="none";
-        popup.parentNode.removeChild(popup);
-    },2000);
-
-
-        } else {
-            showErrorPopup('Failed to add product to cart. Please try again later.');
-        }
-
-    } catch (error) {
-        showErrorPopup('Failed to add product to cart');
-    }
-}
-
-async function addToWishlist(productId) {
-    try {
-        const response = await fetch('/user/wishlist/add', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ productId })
-        });
-        const data = await response.json();
-        if (response.ok) {
-
-            // showPopup('Product added to wishlist successfully!');
-            var popup = document.createElement("div");
-    popup.classList.add("popupMessage");
-    popup.textContent= "Added to wishlist";
-    document.body.appendChild(popup);
+//             var popup = document.createElement("div");
+//     popup.classList.add("popupMessage");
+//     popup.textContent= "Added to cart";
+//     document.body.appendChild(popup);
 
    
-    setTimeout(function(){
-        popup.style.display="block";
-    },100);
 
-    setTimeout(function(){
-        popup.style.display="none";
-        popup.parentNode.removeChild(popup);
-    },2000);
+//     setTimeout(function(){
+//         popup.style.display="block";
+//     },100);
 
-        } else {
-            showErrorPopup('Failed to add product to wishlist. Please try again later.');
-        }
-    } catch (error) {
-        console.error('Error adding to wishlist:', error);
-    }
-}
+//     setTimeout(function(){
+//         popup.style.display="none";
+//         popup.parentNode.removeChild(popup);
+//     },2000);
+
+
+//         } else {
+//             showErrorPopup('Failed to add product to cart. Please try again later.');
+//         }
+
+//     } catch (error) {
+//         showErrorPopup('Failed to add product to cart');
+//     }
+// }
+
+// async function addToWishlist(productId) {
+//     try {
+//         const response = await fetch('/user/wishlist/add', {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             },
+//             body: JSON.stringify({ productId })
+//         });
+//         const data = await response.json();
+//         if (response.ok) {
+
+//             // showPopup('Product added to wishlist successfully!');
+//             var popup = document.createElement("div");
+//     popup.classList.add("popupMessage");
+//     popup.textContent= "Added to wishlist";
+//     document.body.appendChild(popup);
+
+   
+//     setTimeout(function(){
+//         popup.style.display="block";
+//     },100);
+
+//     setTimeout(function(){
+//         popup.style.display="none";
+//         popup.parentNode.removeChild(popup);
+//     },2000);
+
+//         } else {
+//             showErrorPopup('Failed to add product to wishlist. Please try again later.');
+//         }
+//     } catch (error) {
+//         console.error('Error adding to wishlist:', error);
+//     }
+//}
+
+const Items = document.querySelectorAll('.item');
+
+    Items.forEach(item => {
+        const questionButtonDiv = item.querySelector('.question-button-div');
+        questionButtonDiv.addEventListener('click', () => {
+            item.classList.toggle('active');
+        });
+    });
