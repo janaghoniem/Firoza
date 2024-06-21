a96bf18dbedb07f440639d989a14315b72fedf1d
shahdcode <134103174+shahdcode@users.noreply.github.com>
2024-06-21 04:25:27 +0300

    adjustments fel css

--- public/Javascript/adminNavbar.js
+++ public/Javascript/adminNavbar.js
@@ -2,3 +2,11 @@ function toggleMenu(){
     var nav= document.querySelector('.navigation');
     nav.style.display=nav.style.display==='block'? 'none' :'block';
 }
+window.addEventListener('resize', function() {
+    var nav = document.querySelector('.navigation');
+    if (window.innerWidth > 900) {
+        nav.style.display = 'block'; // Ensure the nav is displayed when the screen size is above 900px
+    } else {
+        nav.style.display = 'none'; // Hide the nav when the screen size is below 900px
+    }
+});
